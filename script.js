5ded799728e490deb60835bec5178c721b76d3b5
bmalkmus <bmalkmus23@gmail.com>
2020-01-17 18:31:22 -0800

    current and 5-day forecast

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let citySearch
 let searchHistory
 let historyList
-
+let date = moment().format("MM/DD/YYYY")
+let forecast
 
 if (!localStorage.getItem("city")) {
     
@@ -9,13 +10,15 @@ if (!localStorage.getItem("city")) {
     
     localStorage.setItem('cities', JSON.stringify(searchHistory));
 };
+
 function listUpdate(){
 $('#search-results').empty();
 historyList = JSON.parse(localStorage.getItem("cities"));
+historyList = $(historyList).slice(-10);
     for (i = 0; i < historyList.length; i++){
-        console.log(historyList[i]);
-        $('#search-results').append(historyList[i]+"<br>");
-    }};
+        $('#search-results').prepend(historyList[i]+"<hr>");
+    }
+};
 
 
 $("#search").click(function(){
@@ -24,11 +27,64 @@ $("#search").click(function(){
     searchHistory = JSON.parse(localStorage.getItem("cities"));
     searchHistory.push(citySearch);
     localStorage.setItem('cities', JSON.stringify(searchHistory));
+    $("#5-day").empty();
     listUpdate();
+    currentInfo();
+    forecastInfo();
 })
+  
+function currentInfo(){
+    let queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch +"&units=imperial&appid=945dc48fe14dbea48344eae4427f193e"
+    $.ajax({
+        url: queryURLCurrent,
+        method: "GET"
+      })
+      
+    .then(function(response) {
+          currentIcon = $('<img>')
+          currentIcon.attr("src", "http://openweathermap.org/img/wn/"+response.weather[0].icon+"@2x.png");
+          let currentSelection = $("<h3>").text(citySearch + " (" + date +")");
+          let temperature = $('<p>').text("Temperature: " + response.main.temp + " &#8457;")
+          let windspeed = $('<p>').text("Windspeed: " + response.wind.speed + " MPH")
+            let humidity = $('<p>').text("Humidity:  " + response.main.humidity + "%")
+            $('#current-info').empty();
+          $('#current-info').append(currentSelection);
+          $('#current-info').append(temperature);
+          $('#current-info').append(humidity);
+          $('#current-info').append(windspeed);
+
+
+    })}
+
+function forecastInfo(){
+        let queryURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&appid=945dc48fe14dbea48344eae4427f193e"
+        $.ajax({
+        url: queryURLFiveDay,
+        method: "GET"
+      })
+      
+      .then(function(response) {
+        forecast = [];
+        for(i = 4; i < response.list.length; i+=8){
+            forecast.push(response.list[i])
+        }
+        
+        $(forecast).each(function(){
+            let dailyDiv = $("<div>")
+            let dateFC = moment(this.dt_txt, "YYYY-MM-DD hh:mm:ss").format("MM/DD/YY");
+            let Icon = $('<img>');
+            Icon.attr("src", "http://openweathermap.org/img/wn/"+this.weather[0].icon+"@2x.png");
+            let foreTemp = $('<p>').text("Temperature: " + this.main.temp + " &#8457;");
+            let foreHumidity = $('<p>').text("Humidity:  " + this.main.humidity + "%")
+            $(dailyDiv).append(dateFC);
+            $(dailyDiv).append(Icon);
+            $(dailyDiv).append(foreTemp);
+            $(dailyDiv).append(foreHumidity);
+            $("#5-day").append(dailyDiv);
+
+
+    })
+})
+      }
 
 listUpdate();
-
-
-
-
